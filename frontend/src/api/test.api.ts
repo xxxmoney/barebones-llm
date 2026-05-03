48ef16a220a901f273422d5781419452a388649a
xxxmoney <xxxmoney111@gmail.com>
2026-05-03 15:42:53 +0200

    TS typing for test api fe handler

--- frontend/src/api/test.api.ts
+++ frontend/src/api/test.api.ts
@@ -1,12 +1,13 @@
 import { api } from './index';
+import type {AxiosResponse} from 'axios';
 
 const URL_PREFIX = '/api/test';
 
 export const TestApi = {
-  testConnection() {
+  testConnection(): Promise<AxiosResponse> {
     return api.get(`${URL_PREFIX}/test`);
   },
-  getSampleData() {
+  getSampleData(): Promise<AxiosResponse> {
     return api.get(`${URL_PREFIX}/sample`);
   }
 };
