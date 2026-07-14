f8d9a666c115f65a4cc12bef62736c5cb53300cc
jakub.hana <jakub.hana@sprinx.com>
2026-07-14 09:48:23 +0200

    Use different default model

--- backend/src/constants/llm_constants.py
+++ backend/src/constants/llm_constants.py
@@ -1,6 +1,6 @@
 
 DEFAULT_OPEN_AI_URL = "http://localhost:1234/v1"
 DEFAULT_AI_TOKEN = "lm-studio"
-DEFAULT_MODEL = "magidonia-24b-v4.3-absolute-heresy-i1"
+DEFAULT_MODEL = "qwen/qwen3-vl-4b"
 DEFAULT_MAX_TOKENS = 4096
 DEFAULT_TEMPERATURE = 0.7
