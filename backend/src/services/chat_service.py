3483f8104db1f8522645129372ceadecd27fd5a2
xxxmoney <xxxmoney111@gmail.com>
2026-08-27 15:37:15 +0200

    Usage of mappings

--- backend/src/services/chat_service.py
+++ backend/src/services/chat_service.py
@@ -15,12 +15,12 @@ from src.services import llm_service
 def get_chats() -> List[ChatDto]:
     models = chat_repository.get_chats()
 
-    return [ChatDto(id=model.id, name=model.name, creation_date=model.creation_date, update_date=model.creation_date) for model in models]
+    return [ChatDto.model_validate(model) for model in models]
 
 def get_messages(chat_id: UUID) -> List[MessageDto]:
     model = chat_repository.get_chat(chat_id)
 
-    return [MessageDto(id=message.id, text=message.text, role=message.role, creation_date=message.creation_date, update_date=message.update_date) for message in model.messages]
+    return [MessageDto.model_validate(message) for message in model.messages]
 
 def update_chat(chat_id: UUID, update: ChatUpdateDto) -> ChatDto:
     def update_function(chat: ChatModel):
@@ -29,12 +29,12 @@ def update_chat(chat_id: UUID, update: ChatUpdateDto) -> ChatDto:
 
     model: ChatModel = chat_repository.update_chat(chat_id, update_function)
 
-    return ChatDto(id=model.id, name=model.name, creation_date=model.creation_date, update_date=model.update_date)
+    return ChatDto.model_validate(model)
 
 def insert_chat(update: ChatUpdateDto) -> ChatDto:
     model: ChatModel = chat_repository.insert_chat(ChatModel(id=uuid4(), name=update.name, messages=[], creation_date=datetime.now(UTC), update_date=None))
 
-    return ChatDto(id=model.id, name=model.name, creation_date=model.creation_date, update_date=model.update_date)
+    return ChatDto.model_validate(model)
 
 def delete_chat(chat_id: UUID) -> UUID:
     id = chat_repository.delete_chat(chat_id)
@@ -68,9 +68,7 @@ def insert_messages(chat_id: UUID, messages: List[MessageUpdateDto]):
         [MessageModel(id=uuid4(), text=insert.text, role=insert.role, creation_date=now, update_date=None) for insert in messages]
     )
 
-    return [MessageDto(id=message.id, text=message.text, role=message.role, creation_date=message.creation_date,
-                       update_date=message.update_date) for message in [*inserted]]
-
+    return [MessageDto.model_validate(message) for message in [*inserted]]
 
 def update_message(chat_id: UUID, message_id: UUID, message: MessageUpdateDto) -> MessageDto:
     now = datetime.now(UTC)
@@ -82,7 +80,7 @@ def update_message(chat_id: UUID, message_id: UUID, message: MessageUpdateDto) -
         model.update_date = now
     model = chat_repository.update_chat_message(chat_id, message_id, update_function)
 
-    return MessageDto(id=model.id, text=model.text, role=model.role, creation_date=model.creation_date, update_date=model.update_date)
+    return MessageDto.model_validate(model)
 
 def delete_message(chat_id: UUID, message_id: UUID):
     ids = chat_repository.delete_chat_messages(chat_id, [message_id])
--- backend/src/services/configuration_service.py
+++ backend/src/services/configuration_service.py
@@ -9,13 +9,9 @@ def get_configuration() -> ConfigurationDto:
     return ConfigurationDto.model_validate(config)
 
 def update_configuration(configuration: ConfigurationUpdateDto) -> ConfigurationDto:
-    config = configuration_repository.update_configuration(ConfigurationModel(
-        open_ai_url=configuration.open_ai_url,
-        open_ai_token=configuration.open_ai_token,
-        model=configuration.model,
-        max_tokens=configuration.max_tokens,
-        temperature=configuration.temperature
-    ))
+    config = configuration_repository.update_configuration(
+        ConfigurationModel.model_validate(configuration)
+    )
 
     return ConfigurationDto.model_validate(config)
 
