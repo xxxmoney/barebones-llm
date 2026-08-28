ac7f78d705462a9f11bbd9f2ba8cf077108ffab5
xxxmoney <xxxmoney111@gmail.com>
2026-08-28 19:24:13 +0200

    No use writeback

--- backend/src/services/persistence.py
+++ backend/src/services/persistence.py
@@ -12,9 +12,6 @@ class Persistence():
 
     def __enter__(self) -> "Persistence":
         self._path.parent.mkdir(parents=True, exist_ok=True)
-
-        # TODO: should use writeback or not?
-        #self.db = shelve.open(str(self._path), writeback=True)
         self.db = shelve.open(str(self._path))
 
         return self
