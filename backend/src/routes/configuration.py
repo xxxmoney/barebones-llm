1a36b93ded96b2b25fce87a2e01744b7779135ae
xxxmoney <xxxmoney111@gmail.com>
2026-08-28 12:31:51 +0200

    Fix update configuration validable return

--- backend/src/routes/configuration.py
+++ backend/src/routes/configuration.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter, Body, Response, status
 from src.docs.configuration_docx import UPDATE_CONFIGURATION_EXAMPLES
 from src.dtos.configuration.configuration import ConfigurationDto
 from src.dtos.configuration.configuration_update import ConfigurationUpdateDto
+from src.dtos.validation import ValidableDto
 from src.services import configuration_service
 
 configuration_route = APIRouter(prefix="/api/configuration", tags=["Configuration"])
@@ -12,14 +13,14 @@ def get_configuration() -> ConfigurationDto:
     return configuration_service.get_configuration()
 
 @configuration_route.put("/")
-def update_configuration(response: Response, configuration: ConfigurationUpdateDto = Body(openapi_examples=UPDATE_CONFIGURATION_EXAMPLES)) -> ConfigurationDto:
+def update_configuration(response: Response, configuration: ConfigurationUpdateDto = Body(openapi_examples=UPDATE_CONFIGURATION_EXAMPLES)) -> ValidableDto[ConfigurationDto]:
     validation = configuration_service.validate_configuration_update(configuration)
     configuration = configuration_service.update_configuration(configuration, validation.is_valid)
 
     if not validation.is_valid:
         response.status_code = status.HTTP_400_BAD_REQUEST
 
-    return configuration
+    return ValidableDto(value=configuration, validation=validation)
 
 @configuration_route.delete("/")
 def delete_configuration() -> None:
