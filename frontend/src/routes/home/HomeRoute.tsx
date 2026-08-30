059d507d75fe16c56afb8371d938d45ebaa70b08
xxxmoney <xxxmoney111@gmail.com>
2026-08-30 15:37:17 +0200

    Tidying up

--- frontend/src/routes/home/HomeRoute.tsx
+++ frontend/src/routes/home/HomeRoute.tsx
@@ -8,7 +8,7 @@ function HomeRoute() {
         <div className="hero-content text-center">
           <div className="">
             <h1 className="text-heading"><span className="uppercase font-extrabold">Bare</span>bones LLM</h1>
-            <p className="py-lg">Let's chat with local LLM - as simple as it can get</p>
+            <p className="py-lg">Let's chat with LLM - as simple as it can get</p>
             <Link to="/chats" className="btn btn-primary">LET'S DO IT</Link>
           </div>
         </div>
