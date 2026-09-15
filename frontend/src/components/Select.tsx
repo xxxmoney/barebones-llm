7dfb4482c58d416437028d3c4ffc9be6b328033a
xxxmoney <xxxmoney111@gmail.com>
2026-09-15 16:30:11 +0200

    Fix reset model on url change

--- frontend/src/components/Select.tsx
+++ frontend/src/components/Select.tsx
@@ -2,6 +2,7 @@ import type { ChangeEvent } from 'react';
 
 interface SelectProps {
     defaultValue?: string;
+    value?: string;
     placeholder?: string;
     name?: string;
     required?: boolean;
@@ -11,15 +12,15 @@ interface SelectProps {
     change?: (value: string) => Promise<void>;
 }
 
-function Select({ defaultValue, placeholder, name, required, disabled, className, options, change }: SelectProps) {
+function Select({ defaultValue, value, placeholder, name, required, disabled, className, options, change }: SelectProps) {
   async function handleChange(event: ChangeEvent<HTMLSelectElement>) {
     await change?.(event.currentTarget.value);
   }
 
   return (
     <>
-      <select defaultValue={defaultValue} disabled={disabled} name={name} required={required} onChange={handleChange} className={`select ${className}`}>
-        {placeholder && <option disabled selected value="">{placeholder}</option>}
+      <select key={defaultValue ?? 'reset'} defaultValue={defaultValue ?? ''} value={value} disabled={disabled} name={name} required={required} onChange={handleChange} className={`select ${className}`}>
+        {placeholder && <option disabled value="">{placeholder}</option>}
         {options?.map(option =>
           <option value={option} key={option}>
             {option}
