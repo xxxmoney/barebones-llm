4e2139c381602ea5c570fd15d1d390582d755f34
xxxmoney <xxxmoney111@gmail.com>
2026-09-15 15:40:03 +0200

    Fix new line, white space characters, etc

--- frontend/src/components/EditableText.tsx
+++ frontend/src/components/EditableText.tsx
@@ -5,44 +5,41 @@ interface EditableTextProps {
    text?: string;
    maxLength?: number;
    allowEnterNewLine?: boolean;
+   allowPaste?: boolean;
    className?: string;
    update: (text: string) => Promise<void>;
    clear?: () => Promise<void>;
 }
 
-function EditableText({ text, maxLength, allowEnterNewLine, className, update, clear }: EditableTextProps) {
-
+function EditableText({ text, maxLength, allowEnterNewLine, allowPaste, className, update, clear }: EditableTextProps) {
   async function handleKeyDown(event: KeyboardEvent<HTMLSpanElement>) {
     const isWithinMaxLength = !maxLength || event.currentTarget.textContent.length <= maxLength;
     const isDelete = event.key === 'Backspace' || event.key === 'Delete';
     const isSelectAll = (event.ctrlKey || event.metaKey) && event.key === 'a';
+    const isEnter = event.key === 'Enter';
 
     const preventInput = !isWithinMaxLength && !isDelete && !isSelectAll;
-    const preventEnter = !allowEnterNewLine && event.key === 'Enter';
+    const preventEnter = !allowEnterNewLine && isEnter;
     if (preventInput || preventEnter) {
       event.preventDefault();
     }
-
-    if (preventEnter) {
-      event.currentTarget.blur();
-      if (event.currentTarget.textContent !== text) {
-        await update(event.currentTarget.textContent);
-      }
-    }
   }
   function handlePaste(event: ClipboardEvent<HTMLSpanElement>) {
-    event.preventDefault();
+    const preventPaste = !allowPaste;
+    if (preventPaste) {
+      event.preventDefault();
+    }
   }
   async function handleBlur(event: FocusEvent<HTMLSpanElement>) {
-    if (event.currentTarget.textContent !== text) {
-      await update(event.currentTarget.textContent);
+    if (event.currentTarget.innerText !== text) {
+      await update(event.currentTarget.innerText);
     }
   }
 
   return (
     <>
       <span onKeyDown={handleKeyDown} onPaste={handlePaste} onBlur={handleBlur} className={`relative group ${className}`} contentEditable suppressContentEditableWarning>
-        {text}
+        <span dangerouslySetInnerHTML={{ __html: text! }} className="whitespace-pre-wrap" />
         {clear && <Delete click={clear} absolute className="opacity-0 group-hover:opacity-100" />}
       </span>
     </>
